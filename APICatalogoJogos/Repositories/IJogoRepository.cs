3e874c71bfde045874ab3c4fc2da3dc297f2f59e
wallacesw11 <wallacecarfreak@hotmail.com>
2021-08-10 22:25:06 -0300

    upando arquivos

---
+++ APICatalogoJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogoJogos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogoJogos.Repositories
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task<Jogo> Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);        
+        Task Remover(Guid id);
+    }
+}
---
+++ APICatalogoJogos/Services/JogoService.cs
@@ -0,0 +1,55 @@
+﻿using APICatalogoJogos.InputModel;
+using APICatalogoJogos.Repositories;
+using APICatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogoJogos.Services
+{
+    public class JogoService : IJogoService
+    {
+        private readonly IJogoRepository _jogoRepository;
+
+        public JogoService(IJogoRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
+
+
+        public Task<List<JogoViewModel>> Obter(int pagina, int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task Atualizar(Guid id, JogoInputModel jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(Guid id, double preco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<JogoViewModel> Inserir(JogoInputModel jogo)
+        {
+            throw new NotImplementedException();
+        }
+
+     
+
+        public Task<JogoViewModel> Obter(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
